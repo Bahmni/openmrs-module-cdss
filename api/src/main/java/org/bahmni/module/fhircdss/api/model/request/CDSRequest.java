8487d65879387a3b557b4731e884bb2512440780
Siva Reddy <sivareddy@outlook.com>
2023-04-13 14:04:48 +0530

    BS-102 | Vijay, Siva | Adding Privilege to access CDSS service

--- api/src/main/java/org/bahmni/module/fhircdss/api/model/request/CDSRequest.java
+++ api/src/main/java/org/bahmni/module/fhircdss/api/model/request/CDSRequest.java
@@ -2,13 +2,11 @@ package org.bahmni.module.fhircdss.api.model.request;
 
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.io.Serializable;
 
 
 @Getter
-@Setter
 @Builder
 public class CDSRequest implements Serializable {
 
--- api/src/main/java/org/bahmni/module/fhircdss/api/service/CdssOrderSelectService.java
+++ api/src/main/java/org/bahmni/module/fhircdss/api/service/CdssOrderSelectService.java
@@ -2,11 +2,13 @@ package org.bahmni.module.fhircdss.api.service;
 
 import org.bahmni.module.fhircdss.api.model.alert.CDSCard;
 import org.hl7.fhir.r4.model.Bundle;
+import org.openmrs.annotation.Authorized;
 
 import java.util.List;
 
 public interface CdssOrderSelectService {
     public static final String CDSS_SERVER_BASE_URL_GLOBAL_PROP = "cdss.fhir.baseurl";
 
+    @Authorized("Execute CDSS")
     List<CDSCard> validateInteractions(String serviceName, Bundle bundle);
 }

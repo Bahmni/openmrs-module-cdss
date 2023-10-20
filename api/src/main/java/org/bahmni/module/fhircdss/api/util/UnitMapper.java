e296734e6debeab13fda5d7f5ed122b064d65fb6
manimaarans <110804183+manimaarans@users.noreply.github.com>
2023-10-20 18:36:30 +0530

    BS 239 | BS272 |  Mani, Vijay | Added Fhir conversion logic for Frequency and Dosage unit and added unit test cases for that (#7)
    * BS 239 | Mani, Vijay | Added Fhir conversion logic for Frequency and Dosage unit and added unit test cases for that
    
    * BS 272 | Mani, Vijay | Removed static method and addressed pr comment
    
    * BS 272 | Mani, Vijay | updated Puffs Actuation mapping

---
+++ api/src/main/java/org/bahmni/module/fhircdss/api/util/UnitMapper.java
@@ -0,0 +1,49 @@
+package org.bahmni.module.fhircdss.api.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum UnitMapper {
+
+    L_L("l", "L"),
+    ML_ML("ml", "mL"),
+    UL_UL("ul", "uL"),
+    TABLET_TABLET("Tablet(s)", "Tablet"),
+    CAPSULE_CAPSULE("Capsule(s)", "Capsule"),
+    PUFF_PUFF("Puff(s)", "Actuation"),
+    DROP_DROP("Drop", "Drop"),
+    TABLESPOON_SPOONFUL("Tablespoon", "Spoonful"),
+    TEASPOON("Teaspoon", "Spoonful"),
+    UNIT_CAPSULE("Unit(s)", "Capsule");
+
+
+    private static final Map<String, String> BY_UNIT = new HashMap<>();
+
+    static {
+        for (UnitMapper e : values()) {
+            BY_UNIT.put(e.inputUnit, e.getTargetUnit());
+        }
+    }
+
+    public final String inputUnit;
+    public final String targetUnit;
+
+    private UnitMapper(String inputUnit, String targetUnit) {
+        this.inputUnit = inputUnit;
+        this.targetUnit = targetUnit;
+    }
+
+    public static String factorOfConversion(String sourceUnit) {
+        return BY_UNIT.get(sourceUnit);
+    }
+
+    //getter
+    public String getInputUnit() {
+        return inputUnit;
+    }
+
+
+    public String getTargetUnit() {
+        return targetUnit;
+    }
+}

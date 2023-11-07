6fe30246188bb4a091be835e8aaf459d543d86bc
manimaarans <manimaaran@beehyv.com>
2023-11-07 17:09:32 +0530

    BS 288 | Mani| throw mismatched exceptions when dose unit doesn't match

--- omod/src/main/java/org/bahmni/module/fhircdss/web/controller/CdssValidationController.java
+++ omod/src/main/java/org/bahmni/module/fhircdss/web/controller/CdssValidationController.java
@@ -1,11 +1,17 @@
 package org.bahmni.module.fhircdss.web.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.bahmni.module.fhircdss.api.exception.CdssException;
+import org.bahmni.module.fhircdss.api.exception.DrugDosageException;
 import org.bahmni.module.fhircdss.api.model.alert.CDSAlert;
 import org.bahmni.module.fhircdss.api.service.CdssOrderSelectService;
 import org.hl7.fhir.r4.model.Bundle;
+import org.openmrs.module.webservices.rest.SimpleObject;
 import org.openmrs.module.webservices.rest.web.RestConstants;
 import org.openmrs.module.webservices.rest.web.v1_0.controller.BaseRestController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,8 +30,16 @@ public class CdssValidationController extends BaseRestController {
 
     @PostMapping
     @ResponseBody
-    public List<CDSAlert> validate(@RequestParam(value = "service") String serviceName, @RequestBody Bundle bundle) {
-        return orderSelectService.validateInteractions(serviceName, bundle);
+    public ResponseEntity<?> validate(@RequestParam(value = "service") String serviceName, @RequestBody Bundle bundle) {
+        try {
+            return new ResponseEntity<>(orderSelectService.validateInteractions(serviceName, bundle), HttpStatus.ACCEPTED);
+        } catch (DrugDosageException e) {
+            SimpleObject response = new SimpleObject();
+            response.add("error", e.getMessage());
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        } catch (JsonProcessingException e) {
+            throw new CdssException(e.getMessage());
+        }
     }
 
 }
